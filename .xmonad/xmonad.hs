4e5d69f5b5a43fc3f958d0f873377bfd928340c8
Gilberto <calderongilberto3@gmail.com>
2021-05-15 15:36:53 -0300

    remove unused scripts

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -12,10 +12,10 @@ import XMonad.Util.SpawnOnce
 _terminal           = "kitty"
 _focusFollowsMouse  = True
 _clickJustFocuses   = False
-_borderWidth        = 2
+_borderWidth        = 1
 _modMask            = mod4Mask
 _workspaces         = [" 1 "," 2 "," 3 "," 4 "," 5 "," 6 "," 7 "," 8 "," 9 "]
-_normalBorderColor  = "#dddddd"
+_normalBorderColor  = "#282C34"
 _focusedBorderColor = "#61AFEF"
 
 _keys conf@XConfig { XMonad.modMask = modm } = M.fromList $
@@ -50,7 +50,6 @@ _keys conf@XConfig { XMonad.modMask = modm } = M.fromList $
     , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
     , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
-    , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
     , ( (modm, xK_f ) , spawn "pcmanfm" )
     ]
     ++
@@ -74,7 +73,7 @@ _mouseBindings XConfig {XMonad.modMask = modm} = M.fromList
                                        >> windows W.shiftMaster)
     ]
 
-_layout = tiled ||| Mirror tiled ||| Full
+_layout = tiled ||| Full
   where
     tiled   = Tall nmaster delta ratio
     nmaster = 1
@@ -93,7 +92,7 @@ _manageHook = composeAll
     , title     =? "Android Emulator - emulator:5554"    --> doFloat
     ]
 
-_eventHook   = mempty
+_handleEventHook   = mempty
 _logHook     = return ()
 _startupHook =  return ()
 
@@ -102,7 +101,6 @@ _PP          = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "[" "]" }
 
 _toggleStrutsKey XConfig { XMonad.modMask = modMask } = (modMask, xK_b)
 
-main     = xmonad =<< statusBar _bar _PP _toggleStrutsKey defaults
 defaults = def {
         terminal           = _terminal,
         focusFollowsMouse  = _focusFollowsMouse,
@@ -116,56 +114,9 @@ defaults = def {
         mouseBindings      = _mouseBindings,
         layoutHook         = _layout,
         manageHook         = _manageHook,
-        handleEventHook    = _eventHook,
+        handleEventHook    = _handleEventHook,
         logHook            = _logHook,
         startupHook        = _startupHook
     }
 
-help = unlines ["The default modifier key is 'alt'. Default keybindings:",
-    "",
-    "-- launching and killing programs",
-    "mod-Shift-Enter  Launch xterminal",
-    "mod-p            Launch dmenu",
-    "mod-Shift-p      Launch gmrun",
-    "mod-Shift-c      Close/kill the focused window",
-    "mod-Space        Rotate through the available layout algorithms",
-    "mod-Shift-Space  Reset the layouts on the current workSpace to default",
-    "mod-n            Resize/refresh viewed windows to the correct size",
-    "",
-    "-- move focus up or down the window stack",
-    "mod-Tab        Move focus to the next window",
-    "mod-Shift-Tab  Move focus to the previous window",
-    "mod-j          Move focus to the next window",
-    "mod-k          Move focus to the previous window",
-    "mod-m          Move focus to the master window",
-    "",
-    "-- modifying the window order",
-    "mod-Return   Swap the focused window and the master window",
-    "mod-Shift-j  Swap the focused window with the next window",
-    "mod-Shift-k  Swap the focused window with the previous window",
-    "",
-    "-- resizing the master/slave ratio",
-    "mod-h  Shrink the master area",
-    "mod-l  Expand the master area",
-    "",
-    "-- floating layer support",
-    "mod-t  Push window back into tiling; unfloat and re-tile it",
-    "",
-    "-- increase or decrease number of windows in the master area",
-    "mod-comma  (mod-,)   Increment the number of windows in the master area",
-    "mod-period (mod-.)   Deincrement the number of windows in the master area",
-    "",
-    "-- quit, or restart",
-    "mod-Shift-q  Quit xmonad",
-    "mod-q        Restart xmonad",
-    "mod-[1..9]   Switch to workSpace N",
-    "",
-    "-- Workspaces & screens",
-    "mod-Shift-[1..9]   Move client to workspace N",
-    "mod-{w,e,r}        Switch to physical/Xinerama screens 1, 2, or 3",
-    "mod-Shift-{w,e,r}  Move client to screen 1, 2, or 3",
-    "",
-    "-- Mouse bindings: default actions bound to mouse events",
-    "mod-button1  Set the window to floating mode and move by dragging",
-    "mod-button2  Raise the window to the top of the stack",
-    "mod-button3  Set the window to floating mode and resize by dragging"]
+main     = xmonad =<< statusBar _bar _PP _toggleStrutsKey defaults
