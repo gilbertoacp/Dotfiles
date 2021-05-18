8803c1f924f027363adf37d5a8fb3ebaded3981f
Gilberto <calderongilberto3@gmail.com>
2021-05-18 13:31:38 -0300

    [xmonad] fix fullscreen behavior

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,6 +3,9 @@ import Data.Monoid
 import System.Exit
 import Graphics.X11.ExtraTypes.XF86
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat, doCenterFloat)
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.NoBorders
 
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
@@ -16,7 +19,9 @@ _focusFollowsMouse  = True
 _clickJustFocuses   = False
 _borderWidth        = 1
 _modMask            = mod4Mask
-_workspaces         = [" 1 "," 2 "," 3 "," 4 "," 5 "," 6 "," 7 "," 8 "," 9 "]
+
+_workspaces =  [" 1 "," 2 "," 3 "," 4 "," 5 "," 6 "," 7 "," 8 "," 9 "]
+
 _normalBorderColor  = "#282C34"
 _focusedBorderColor = "#61AFEF"
 
@@ -27,7 +32,7 @@ _keys conf@XConfig { XMonad.modMask = modm } = M.fromList $
     , ((0, xF86XK_AudioMute ), spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle")
     , ((0, xF86XK_AudioLowerVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ -10%")
     , ((0, xF86XK_AudioRaiseVolume), spawn "pactl set-sink-volume @DEFAULT_SINK@ +10%")
-    , ((modm,               xK_p     ), spawn "dmenu_run")
+    , ((modm,               xK_p     ), spawn "dmenu_run -nf '#F8F8F2' -nb '#282C34' -sb '#61AFEF' -sf '#F8F8F2' -fn 'monospace-10' -p 'Run'")
     , ((modm,               xK_space ), sendMessage NextLayout)
     , ((modm .|. shiftMask, xK_space ), setLayout $ XMonad.layoutHook conf)
     , ((modm,               xK_n     ), refresh)
@@ -43,7 +48,7 @@ _keys conf@XConfig { XMonad.modMask = modm } = M.fromList $
     , ((modm,               xK_t     ), withFocused $ windows . W.sink)
     , ((modm              , xK_comma ), sendMessage (IncMasterN 1))
     , ((modm              , xK_period), sendMessage (IncMasterN (-1)))
-    , ((modm, xK_f) , spawn "pcmanfm")
+    , ((modm, xK_e) , spawn "pcmanfm")
     , ((modm .|. shiftMask, xK_q), kill)
     , ((controlMask .|. shiftMask, xK_r), spawn "xmonad --recompile; xmonad --restart")
     , ((controlMask .|. shiftMask, xK_Escape), io exitSuccess)
@@ -53,40 +58,37 @@ _keys conf@XConfig { XMonad.modMask = modm } = M.fromList $
     [((m .|. modm, k), windows $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
-    ++
+    -- ++
 
-    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
-        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+    -- [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+    --     | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+    --     , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
 _mouseBindings XConfig {XMonad.modMask = modm} = M.fromList
 
-    [ ((modm, button1), \w -> focus w >> mouseMoveWindow w
-                                       >> windows W.shiftMaster)
+    [
+      ((modm, button1), \w -> focus w >> mouseMoveWindow w >> windows W.shiftMaster)
     , ((modm, button2), \w -> focus w >> windows W.shiftMaster)
-
-    , ((modm, button3), \w -> focus w >> mouseResizeWindow w
-                                       >> windows W.shiftMaster)
+    , ((modm, button3), \w -> focus w >> mouseResizeWindow w >> windows W.shiftMaster)
     ]
 
 _layout = tiled ||| Full
   where
-    tiled   = Tall nmaster delta ratio
-    nmaster = 1
-    ratio   = 1/2
-    delta   = 3/100
+    tiled   = Tall  1 (3/100) (3/5)   
 
 _manageHook = composeAll
-    [ className =? "Nitrogen"        --> doFloat
+    [ className =? "Nitrogen"        --> doCenterFloat
     , className =? "notification"    --> doFloat
     , className =? "confirm"         --> doFloat
     , className =? "file_progress"   --> doFloat
     , className =? "dialog"          --> doFloat
     , className =? "download"         --> doFloat
-    , className =? "error"                   --> doFloat
-    , className =? "Qalculate-gtk"           --> doFloat
-    , className =? "SimpleScreenRecorder" --> doFloat
-    , title     =? "Android Emulator - emulator:5554"    --> doFloat
+    , className =? "error"                   --> doCenterFloat
+    , className =? "Qalculate-gtk"           --> doCenterFloat
+    , className =? "SimpleScreenRecorder" --> doCenterFloat
+    , className =? "mpv" --> doFullFloat
+    , title     =? "Emulator"    --> doCenterFloat
+    , isFullscreen -->  doFullFloat
     ]
 
 _handleEventHook   = fullscreenEventHook
@@ -95,7 +97,7 @@ _startupHook =  do
     spawnOnce "trayer --edge top --align right --widthtype request --padding 6 --SetDockType true --SetPartialStrut true --expand true --monitor 1 --transparent true --alpha 0 --tint 0x282c34  --height 20 &"
 
 _bar         = "xmobar"
-_PP          = xmobarPP { 
+_PP          = xmobarPP {
     ppCurrent = xmobarColor "#61AFEF" "" . wrap "[" "]",
     ppTitle   = xmobarColor "#ABB2BF" "" . shorten 60,
     ppSep = "<fc=#666> | </fc>"
@@ -114,7 +116,7 @@ defaults = def {
         focusedBorderColor = _focusedBorderColor,
         keys               = _keys,
         mouseBindings      = _mouseBindings,
-        layoutHook         = _layout,
+        layoutHook         =  avoidStruts $ smartBorders _layout,
         manageHook         = _manageHook,
         handleEventHook    = _handleEventHook,
         logHook            = _logHook,
