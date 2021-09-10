b1692a35e1025f78a11130248ed3909ea6a88df2
Gilberto <calderongilberto3@gmail.com>
2021-09-10 03:00:28 -0300

    nvim: migrating to lua

---
+++ .config/nvim/lua/keymaps.lua
@@ -0,0 +1,87 @@
+vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', {
+    noremap = true,
+    silent  = true
+})
+vim.g.mapleader = ' '
+
+vim.api.nvim_set_keymap('n', '<Leader>h', ':set hlsearch!<CR>', { 
+    noremap = true,
+    silent  = true
+})
+
+vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', { 
+    noremap = true,
+    silent  = true
+})
+vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { 
+    silent  = true
+})
+vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { 
+    silent  = true
+})
+vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { 
+    silent  = true
+})
+vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { 
+    silent  = true
+})
+
+vim.api.nvim_set_keymap('v', '<', '<gv', { 
+    noremap = true,
+    silent  = true
+})
+vim.api.nvim_set_keymap('v', '>', '>gv', { 
+    noremap = true,
+    silent  = true
+})
+
+vim.api.nvim_set_keymap('n', '<TAB>', ':bnext<CR>', { 
+    noremap = true,
+    silent  = true
+})
+
+vim.api.nvim_set_keymap('n', '<S-TAB>', ':bprevious<CR>', { 
+    noremap = true,
+    silent  = true
+})
+
+
+vim.api.nvim_set_keymap('v', 'K', ':move \'<-2<CR>gv-gv\'', { 
+    noremap = true,
+    silent  = true
+})
+vim.api.nvim_set_keymap('v', 'J', ':move \'>+2<CR>gv-gv\'', { 
+    noremap = true,
+    silent  = true
+})
+
+
+vim.api.nvim_set_keymap('v', 'J', ':move \'>+2<CR>gv-gv\'', { 
+    noremap = true,
+    silent  = true
+})
+
+vim.api.nvim_set_keymap('i', '<expr><TAB>', [[pumvisible() ? "\<C-n>" : "\<TAB>"]], { 
+    noremap = true,
+    silent  = true
+})
+
+
+vim.api.nvim_set_keymap('n', '<C-Up>', ':resize -2<CR>', { 
+    noremap = true,
+    silent  = true
+})
+
+
+vim.api.nvim_set_keymap('n', '<C-Down>', ':resize +2<CR>', { 
+    noremap = true,
+    silent  = true
+})
+vim.api.nvim_set_keymap('n', '<C-Left>', ':vertical resize -2<CR>', { 
+    noremap = true,
+    silent  = true
+})
+vim.api.nvim_set_keymap('n', '<C-Right>', ':vertical resize +2<CR>', { 
+    noremap = true,
+    silent  = true
+})
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,20 @@
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd 'packadd packer.nvim'
+end
+
+return require('packer').startup(function()
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- NVIM TREE
+  use 'kyazdani42/nvim-tree.lua'
+
+  -- Native LSP
+  use 'neovim/nvim-lspconfig'
+--  use'hrsh7th/nvim-compe'
+end)
